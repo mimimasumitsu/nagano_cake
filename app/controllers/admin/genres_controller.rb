d93a9ece38b80ec4baca263b00e757b7d9aebe64
EC2 Default User <ec2-user@ip-172-31-72-185.ec2.internal>
2021-06-30 14:22:50 +0000

    [Update] 管理者ジャンル一覧・追加画面作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,15 +1,27 @@
 class Admin::GenresController < ApplicationController
 
   def index
+    @genres = Genre.all
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :created_at, :updated_at)
   end
 
 end

24906a764a4e52ddad3ef6ad89eac86e95e5a971
EC2 Default User <ec2-user@ip-172-31-72-185.ec2.internal>
2021-07-01 14:47:06 +0000

    [Update] 管理者注文履歴詳細画面作成途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,13 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = OrderItem.find(params[:id])
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:item_id, :order_id, :address, :price, :amount, :making_status, :created_at, :updated_at)
   end
 
 end

bb108e8d71e2ed20ef02e8fc103412e9a5f7b2f4
EC2 Default User <ec2-user@ip-172-31-72-185.ec2.internal>
2021-07-01 14:00:56 +0000

    [Update] 管理者会員詳細画面作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,12 +5,19 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :created_at, :updated_at)
   end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,8 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
+
+  def full_name
+    first_name + last_name
+  end
 end

433a4ac618a728edff8699c6d3ff5effe867a0f6
EC2 Default User <ec2-user@ip-172-31-72-185.ec2.internal>
2021-07-18 16:27:43 +0000

    [Update] 配送先編集画面作成

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,18 +1,35 @@
 class AddressesController < ApplicationController
 
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to addresses_path(@address)
+    end
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path(@address)
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 
 end

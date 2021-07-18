bf01adc4e741caf373b02e035dad30f92b44b5fb
EC2 Default User <ec2-user@ip-172-31-72-185.ec2.internal>
2021-07-18 15:44:29 +0000

    [Update] 会員情報編集画面作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,19 +1,32 @@
 class CustomersController < ApplicationController
 
   def show
-   @customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
+    else
+      render :edit
+    end
   end
 
   def confirm
   end
 
   def cancel
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
 
 end
